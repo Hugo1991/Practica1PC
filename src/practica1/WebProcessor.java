877a8135e6d4d6135eca65207cb2b044f21945ba
Hugo1991 <hugofernandezvisier@gmail.com>
2016-05-14 19:23:47 +0200

    muestraProgreso y max num de descargas

--- src/practica1/WebProcessor.java
+++ src/practica1/WebProcessor.java
@@ -3,6 +3,8 @@ package practica1;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Scanner;
+import java.util.concurrent.Semaphore;
 
 import org.jsoup.Connection;
 import org.jsoup.Connection.Response;
@@ -15,40 +17,47 @@ public class WebProcessor {
 	private int maxDown; // máximo número de procesos que pueden descargar webs
 							// a la vez
 	private int progreso;
+	Semaphore semMaxDescargas;
+	Semaphore semMaxEscritura = new Semaphore(1);
+
 	public WebProcessor(String path, int nDown, int maxDown) {
 		ruta = path;
 		this.nDown = nDown;
 		this.maxDown = maxDown;
-		progreso=0;
+		progreso = 0;
+		new Thread(() -> mostrarProgreso(), "hilo mostrar progreso").start();
+		semMaxDescargas = new Semaphore(maxDown);
 	}
 
 	public void process(String fileName) {
-		
+
 		try {
 			FileReader f = new FileReader(fileName);
 			BufferedReader b = new BufferedReader(f);
 			String linea;
 			linea = b.readLine();
-		
-		
-		while (!(linea == null)||progreso<=nDown) {
-			for(int i=0;i<maxDown;i++){
-				String lineaCopia=linea;
-				new Thread(()->descargaFichero(lineaCopia),"Hilo "+i).start();
+
+			while (!(linea == null) && (progreso <= nDown)) {
+				semMaxDescargas.acquire();
+				String lineaCopia = linea;
+				new Thread(() -> descargaFichero(lineaCopia), "Hilo " + progreso).start();
+				
+
+				linea = b.readLine();
 				progreso++;
-				linea=b.readLine();
 			}
-			
-		}
-		
+
 		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 
 	private void descargaFichero(String url) {
-		
+
 		// Creates a connection to a given url
 		Connection conn = Jsoup.connect(url);
 		try {
@@ -57,22 +66,28 @@ public class WebProcessor {
 			// If the response is different from 200 OK,
 			// the website is not reachable
 			if (resp.statusCode() != 200) {
-				System.out.println("Error: " + resp.statusCode());
+				System.out.println(progreso + " " + url + " error");
 			} else {
 				String html = conn.get().html();
+				System.out.println(" " + url);
 				escribirFichero(url, html, false);
 			}
-		}catch(IllegalArgumentException iae){
+		} catch (IllegalArgumentException iae) {
+			// System.out.println("error error error");
 		} catch (IOException e) {
-			System.out.println("No se puede conectar");
+			escribirFichero("error.txt", url, false);
+			// System.out.println(progreso+" No se puede conectar");
 		}
 	}
-	private void escribirFichero(String fichero, String texto, boolean error){
+
+	private void escribirFichero(String fichero, String texto, boolean error) {
+		// semMaxEscritura.release();
 		
+		semMaxDescargas.release();
 	}
 
-	private void mostrarProgreso(){
-		while(true){
+	private void mostrarProgreso() {
+		while (true) {
 			System.out.println(progreso);
 			try {
 				Thread.sleep(3000);
@@ -82,13 +97,16 @@ public class WebProcessor {
 			}
 		}
 	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Introduce el fichero con las páginas web que quieres descargar");
-		String fichero="3.txt";
-		String ruta="descargas";
-		WebProcessor wp=new WebProcessor(ruta, 400, 10);
-		wp.process(fichero);
+		String ficheroEntrada = new Scanner(System.in).nextLine();
+		System.out.println("Introduce el directorio donde quieres guardar las descargas");
+		String ruta = new Scanner(System.in).nextLine();
+		WebProcessor wp = new WebProcessor(ruta, 50, 10);
+		wp.process(ficheroEntrada);
+
 	}
 
 }
